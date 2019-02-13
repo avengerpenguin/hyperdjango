430e4295417276552750dba9787d0afaef3f1e7c
Ross Fenning <ross.fenning@gmail.com>
2019-02-13 00:10:35 +0000

    Travis config stub

---
+++ tests/app.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+
+from django.conf import settings
+import django12factor
+
+
+
+settings.configure(
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+    **django12factor.factorise()
+)
+
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+
+application = get_wsgi_application()
