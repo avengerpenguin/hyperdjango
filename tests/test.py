0b1ca187d94c44dab81341f7744d594ceb84d113
Ross Fenning <ross.fenning@gmail.com>
2019-02-21 00:31:38 +0000

    Prove objects can be updated in the back end

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,7 @@ from .models import Thing
 
 
 @pytest.mark.django_db
-def test(client):
+def test_getting_object(client):
     thing = Thing(name='Berlin')
     thing.save()
 
@@ -19,3 +19,31 @@ def test(client):
 
     assert 'Berlin' in entity.test_name
 
+
+@pytest.mark.django_db
+def test_object_changing_in_backend(client):
+    thing = Thing(name='Giraffe')
+    thing.save()
+
+    r = client.get('/things/1')
+
+    assert r.status_code == 200
+    g = Graph()
+    g.bind('test', Namespace('http://testserver/'))
+    g.parse(data=r.content, format=r['Content-Type'])
+    entity = ThingFactory(g)(Namespace('http://testserver/')['things/1'])
+
+    assert 'Giraffe' in entity.test_name
+
+    thing.name = 'Elephant'
+    thing.save()
+
+    r = client.get('/things/1')
+    
+    assert r.status_code == 200
+    g = Graph()
+    g.bind('test', Namespace('http://testserver/'))
+    g.parse(data=r.content, format=r['Content-Type'])
+    entity = ThingFactory(g)(Namespace('http://testserver/')['things/1'])
+
+    assert 'Elephant' in set(entity.test_name)
