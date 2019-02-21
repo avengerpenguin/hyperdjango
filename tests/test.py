565cc64db4971c59675281fabeee9ce3bd9dd363
Ross Fenning <ross.fenning@gmail.com>
2019-02-21 00:27:39 +0000

    Test model being returned

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,6 @@
 import pytest
+from rdflib import Graph, Namespace
+from laconia import ThingFactory
 from .models import Thing
 
 
@@ -10,3 +12,10 @@ def test(client):
     r = client.get('/things/1')
 
     assert r.status_code == 200
+    g = Graph()
+    g.bind('test', Namespace('http://testserver/'))
+    g.parse(data=r.content, format=r['Content-Type'])
+    entity = ThingFactory(g)(Namespace('http://testserver/')['things/1'])
+
+    assert 'Berlin' in entity.test_name
+
